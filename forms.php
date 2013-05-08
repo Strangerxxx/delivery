9b6da3a3789337b14d3bf93cccdda572067490bf
Stranger DaNeK <black.danek@gmail.com>
2013-05-09 01:57:17 +0400

    drunk update
    last for this day

--- forms.php
+++ forms.php
@@ -29,9 +29,8 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 	</head>
 <?php 
-	if(isset($_SESSION['sessionid'])){
-		$userid = checkUserLogin($_SESSION['sessionid']);
-		if (!$userid){ ?>
+	if(!isset($_SESSION['sessionid']) && ($userid = checkUserLogin($_SESSION['sessionid'])) == false){
+		?>
 	<form method="POST">
 		<input type="text" name="login" placeholder="Your login...">
 		<input type="password" name="pass" placeholder="Your password...">
@@ -54,5 +53,5 @@
 		<input type="hidden" name="request" value="logout">
 		<input type="submit" value="Logout" name="submit">
 	</form>
-	<?php } } ?>
+	<?php } ?>
 </html>
