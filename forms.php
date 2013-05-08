e64fda21c4840cea13b292ba5aa3895d5c2652a9
Stranger DaNeK <black.danek@gmail.com>
2013-05-09 02:05:32 +0400

    another syntax error

--- forms.php
+++ forms.php
@@ -29,7 +29,7 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 	</head>
 <?php 
-	if(isset($_SESSION['sessionid']) $userid = checkUserLogin($_SESSION['sessionid']);
+	if(isset($_SESSION['sessionid'])) $userid = checkUserLogin($_SESSION['sessionid']);
 	if(!isset($_SESSION['sessionid']) && $userid == false){
 		?>
 	<form method="POST">
